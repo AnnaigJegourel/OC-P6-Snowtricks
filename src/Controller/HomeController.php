b76475f00029c70ec1111d598d31e51db78da2a0
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-12-14 11:13:02 +0100

    Update with false data

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,21 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $allTricks = [
+            "trick1" => [
+                "id" => 1,
+                "name" => "mute",
+                "image"=> "image mute"
+            ],
+            "trick2" => [
+                "id" => 2,
+                "name" => "sad",
+                "image"=> "image sad"
+            ]
+        ];
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'allTricks' => $allTricks
         ]);
     }
 }
