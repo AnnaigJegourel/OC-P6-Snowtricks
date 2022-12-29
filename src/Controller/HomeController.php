41680d4194af15d41f5399c47122a012acdfb128
Annaig Jégourel <a_jegourel@yahoo.fr>
2022-12-29 12:11:19 +0100

    update tricks index adding homepage content & delete homepage view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,22 +12,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home', methods: ['GET'])]
     public function index(TrickRepository $trickRepository): Response
     {
-        /*$allTricks = [
-            "trick1" => [
-                "id" => 1,
-                "name" => "mute",
-                "image"=> "/img/mute.png"
-            ],
-            "trick2" => [
-                "id" => 2,
-                "name" => "flip",
-                "image"=> "/img/flip.jpg"
-            ]
-        ];
-
-        return $this->render('home/index.html.twig', [
-            'allTricks' => $allTricks
-        ]);*/
         return $this->render('trick/index.html.twig', [
             'tricks' => $trickRepository->findAll(),
         ]);
