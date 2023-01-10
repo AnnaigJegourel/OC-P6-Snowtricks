cfdc36ed5c0a13947e071453120ed6df23f76b09
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-01-10 15:40:04 +0100

    make:registration-form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -35,6 +37,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column]
     private ?bool $isActivated = false;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     //#[ORM\Column(nullable: true)]
     //private ?bool $activated = null;
@@ -153,6 +158,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setIsActivated(bool $isActivated): self
     {
         $this->isActivated = $isActivated;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
