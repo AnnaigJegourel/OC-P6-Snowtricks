95c49759f5332aea907f940633f10bd3b4cadce2
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-01-12 13:19:56 +0100

    Update Image entity: making $file (vich) uploadable

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -5,8 +5,13 @@ namespace App\Entity;
 use App\Repository\ImageRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Vich\UploaderBundle\Mapping\Annotation\UploadableField as AnnotationUploadableField;
+
 
 #[ORM\Entity(repositoryClass: ImageRepository::class)]
+#[Vich\Uploadable]
 class Image
 {
     #[ORM\Id]
@@ -17,8 +22,10 @@ class Image
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(type: Types::BINARY)]
-    private $file = null;
+    // #[ORM\Column(type: Types::BINARY)]
+    // private $file = null;
+    #[AnnotationUploadableField(mapping: "trick_picture", fileNameProperty: "name")]
+    private ?File $file = null;
 
     #[ORM\ManyToOne(inversedBy: 'images')]
     #[ORM\JoinColumn(nullable: false)]
