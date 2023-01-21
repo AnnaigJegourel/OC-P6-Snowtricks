480206302e8a7165b744802f2fe7b989966a0461
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-01-21 11:45:52 +0100

    Make Entity/Image $name nullable

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -19,8 +19,9 @@ class Image
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
+    #[ORM\Column(length: 255, nullable:true)]
+    //private ?string $name = null;
+    private ?string $name;
 
     // #[ORM\Column(type: Types::BINARY)]
     // private $file = null;
