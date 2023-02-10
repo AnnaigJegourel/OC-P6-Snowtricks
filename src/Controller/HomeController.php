95c6f718266e05d499d2fce1252681e8ceed602b
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-02-10 09:41:38 +0100

    Use getTrickPaginator() in tricks' index (HomeController)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\TrickRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,11 +11,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(TrickRepository $trickRepository): Response
+    public function index(TrickRepository $trickRepository, Request $request, ): Response
     {
+        //use paginator
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $trickRepository->getTrickPaginator($offset);
+        
         return $this->render('trick/index.html.twig', [
-            'tricks' => $trickRepository->findAll(),
-            'user' => $this->getUser()
+            // 'tricks' => $trickRepository->findAll(),
+            'user' => $this->getUser(),
+            'tricks' => $paginator,
+            'previous' => $offset - TrickRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + TrickRepository::PAGINATOR_PER_PAGE),
         ]);
     }
 }
