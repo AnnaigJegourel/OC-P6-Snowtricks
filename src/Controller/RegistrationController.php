a9faffbf29ff59c1d5abef8b7e7aaba27a71ab30
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-02-19 11:00:28 +0100

    Customize RegistrationController

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -47,6 +47,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
+                    // hide address & name making them as CONSTANTS?
                     ->from(new Address('admin@annaig-jegourel.net', 'Admin'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
@@ -77,8 +78,8 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('notic', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_home');
     }
 }
