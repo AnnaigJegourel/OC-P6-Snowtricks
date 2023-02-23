dab607432e7ccd677de5ab62dd5c6b4d95254aef
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-02-23 15:19:22 +0100

    Make user e-mail unique & add message to username (already unique)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,8 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+#[UniqueEntity(fields: ['username'], message: '--  !! There is already an account with this username !!  --')]
+#[UniqueEntity(fields: ['email'], message: '-- !! There is already an account with this email address !! --')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -31,7 +32,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
     #[ORM\Column(length: 255, nullable: true)]
