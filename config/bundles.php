fc36b96846da4a54339ac8753ec432e06bc9391e
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-02-24 11:04:23 +0100

    composer require zenstruck/foundry --dev

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
