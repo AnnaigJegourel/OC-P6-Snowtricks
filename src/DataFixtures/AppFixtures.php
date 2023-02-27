73d3a1a3058065fcca85c03df6ad2832c0826008
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-02-27 15:23:58 +0100

    Update AppFixtures creating many categories

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategoryFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +12,7 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        CategoryFactory::createMany(2);
 
         $manager->flush();
     }
