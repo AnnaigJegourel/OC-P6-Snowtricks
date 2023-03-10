561d59c6b266464b2103f036f865d196d74429a6
Annaig Jégourel <a_jegourel@yahoo.fr>
2023-03-10 10:37:31 +0100

    Clean src/Security (remove useless lines)

--- src/Security/EmailVerifier.php
+++ src/Security/EmailVerifier.php
@@ -7,7 +7,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
@@ -20,7 +19,6 @@ class EmailVerifier
     ) {
     }
 
-    // public function sendEmailConfirmation(string $verifyEmailRouteName, UserInterface $user, TemplatedEmail $email): void
     public function sendEmailConfirmation(string $verifyEmailRouteName, User $user, TemplatedEmail $email): void
     {
         $signatureComponents = $this->verifyEmailHelper->generateSignature(
@@ -43,7 +41,6 @@ class EmailVerifier
     /**
      * @throws VerifyEmailExceptionInterface
      */
-    // public function handleEmailConfirmation(Request $request, UserInterface $user): void
     public function handleEmailConfirmation(Request $request, User $user): void
     {
         $this->verifyEmailHelper->validateEmailConfirmation($request->getUri(), $user->getId(), $user->getEmail());
